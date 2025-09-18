baf8e8806a6cb0633ddfdd8e8575c7d496ebb587
justt-ashy <aashitabansal10b13@gmail.com>
2025-09-18 22:56:05 +0530

    Updated Project

--- src/main/java/com/example/LMS_Backend/ApplicationStartup.java
+++ src/main/java/com/example/LMS_Backend/ApplicationStartup.java
@@ -1,4 +1,32 @@
 package com.example.LMS_Backend;
 
-public class ApplicationStartup {
+
+import com.example.LMS_Backend.common.Constants;
+import com.example.LMS_Backend.model.User;
+import com.example.LMS_Backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationStartup implements ApplicationListener<ApplicationEvent> {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent){
+        initDatabaseEntities();
+    }
+
+    private void initDatabaseEntities() {
+        if(userService.getAllUsers().size()==0){
+            userService.addNew(new User("Mr. Admin", "admin", "admin", Constants.ROLE_ADMIN));
+            userService.addNew(new User("Mr. Librarian", "librarian", "librarian", Constants.ROLE_LIBRARIAN));
+        }
+    }
+
+
 }
--- src/main/java/com/example/LMS_Backend/LoginSuccessListener.java
+++ src/main/java/com/example/LMS_Backend/LoginSuccessListener.java
@@ -1,4 +1,26 @@
 package com.example.LMS_Backend;
 
-public class LoginSuccessListener {
+import com.example.LMS_Backend.model.User;
+import com.example.LMS_Backend.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginSuccessListener implements ApplicationListener<AuthenticationSuccessEvent> {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HttpSession httpSession;
+
+    @Override
+    public void onApplicationEvent(AuthenticationSuccessEvent event){
+        User user = (User) event.getAuthentication().getPrincipal();
+        String displayName = userService.getByUsername( user.getUsername()).getDisplayName();
+        httpSession.setAttribute("loggedInUserName", displayName);
+    }
 }
